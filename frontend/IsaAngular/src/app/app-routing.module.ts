84a314fb82163c6e57d7c271480589af1926a9fa
vlada <vlada@vlada-Vostro-15-3568>
2018-03-27 20:59:53 +0200

    angular osnovno

---
+++ frontend/IsaAngular/src/app/app-routing.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { CinemasComponent } from './cinemas/cinemas.component';
+import { FanpageComponent } from './fanpage/fanpage.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { TheatresComponent } from './theatres/theatres.component';
+import { UserpageComponent } from './userpage/userpage.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/homepage', pathMatch: 'full'},
+  { path: 'homepage', component: HomepageComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'cinemas', component: CinemasComponent},
+  { path: 'fanpage', component: FanpageComponent},
+  { path: 'theatres', component: TheatresComponent},
+  { path: 'userpage', component: UserpageComponent},
+  { path: '**', component: PageNotFoundComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+export const RoutingComponents = [
+  HomepageComponent,
+  LoginComponent,
+  RegisterComponent,
+  CinemasComponent,
+  FanpageComponent,
+  TheatresComponent,
+  UserpageComponent,
+  PageNotFoundComponent
+];
