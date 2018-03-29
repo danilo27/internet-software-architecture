3b19109e538b1e7bfd24546e495903ba42613bd1
Danilo <danilo.daca.b@gmail.com>
2018-03-29 03:00:40 +0200

    povezan spring i angular

--- src/main/java/com/WelcomeController.java
+++ src/main/java/com/WelcomeController.java
@@ -3,24 +3,18 @@ package com;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.beans.User;
 
-@Controller
+@RestController
 public class WelcomeController {
-//	@Autowired
-//	private IndexService service;
-//	
-//	@RequestMapping("/")
-//	public String getIndex(){
-//		return service.getIndex();
-//	}
+	@GetMapping("/hello")
+	public String sayHello(){
+		return "{\"message\":\"hey\"}";
+	}
 }
 
-//@Component 
-//class IndexService{
-//	public String getIndex(){
-//		return "index";
-//	}
-//}
+
