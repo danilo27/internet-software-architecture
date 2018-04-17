a7c1705ca7cd054755d54ad457c1e0ad0056b6e5
vlada <vlada@vlada-Vostro-15-3568>
2018-04-17 17:15:38 +0200

    popravljen izgled, dodat pregled bioskopa i njihova lokacija na gm

--- src/main/java/com/controllers/RekvizitController.java
+++ src/main/java/com/controllers/RekvizitController.java
@@ -1,8 +1,12 @@
 package com.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.bson.Document;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,9 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.beans.Licitacija;
 import com.beans.PolovniRekvizit;
+import com.beans.PozBio;
 import com.beans.RezervacijaRekvizita;
 import com.beans.User;
 import com.beans.ZvanicniRekvizit;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import com.services.RekvizitiService;
 
 @RestController
@@ -141,6 +151,33 @@ public class RekvizitController {
 		rs.odbijenOglas(oglas);
 	}
 	
-	
+	@GetMapping("/getCinemas")
+	public String getCinemas() {
+		MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
+		MongoDatabase baza = mongoClient.getDatabase("test");
+		MongoCollection<Document> pozbios = baza.getCollection("pozbios");
+		List<Document> pozbioslist = (List<Document>) pozbios.find().into(new ArrayList<Document>());
+		
+		Gson g = new GsonBuilder().create();
+		
+		String ret = "[";
+		
+		boolean dodaj = false;
+		
+		for(Document d : pozbioslist) {
+			if(dodaj) {
+				ret+=",";
+			}
+			dodaj = true;
+			PozBio r = g.fromJson(d.toJson(), PozBio.class);
+			ret+=g.toJson(r);
+		}
+		ret+="]";
+
+		mongoClient.close();
+		
+		return ret;
+		
+	}
 	
 }
